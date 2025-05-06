3f5f137c8f2624b7dbb79ef52dfd27fe50a6eb09
dxlliv <hello@dxlliv.dev>
2025-05-07 01:18:03 +0200

    feat: Add vueuse modules

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,10 +1,16 @@
-import { defineNuxtModule, createResolver, addComponentsDir, addPlugin, addImportsDir } from '@nuxt/kit';
+import {defineNuxtModule, createResolver, addComponentsDir, addPlugin, addImportsDir, installModule} from '@nuxt/kit';
 
 export default defineNuxtModule({
   meta: {
     name: '@gridsky/core'
   },
   async setup(options, nuxt) {
+
+    {
+
+      await installModule('@vueuse/nuxt')
+
+    }
 
   }
 });
