485fc503e8e9b0381374c82620e53577c35c9b83
dxlliv <hello@dxlliv.dev>
2025-05-07 01:07:36 +0200

    fix: Make the vuetify configuration work

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -5,7 +5,58 @@ export default defineNuxtModule({
     name: '@gridsky/ui'
   },
   async setup(options, nuxt) {
-    const { resolve } = createResolver(import.meta.url);
+    const {resolve} = createResolver(import.meta.url);
+
+    {
+
+      nuxt.hook('vuetify:registerModule', register => register({
+        moduleOptions: {
+          styles: 'sass',
+        },
+        vuetifyOptions: {
+          labComponents: ['VPullToRefresh'],
+          theme: {
+            defaultTheme: 'dark',
+          },
+          defaults: {
+            VBtn: {
+              flat: true
+            },
+            VCard: {
+              flat: true
+            },
+            VList: {
+              flat: true
+            },
+            VListItem: {
+              flat: true
+            },
+            VField: {
+              flat: true
+            },
+            VTextField: {
+              flat: true
+            },
+            VTextarea: {
+              flat: true
+            },
+            VMenu: {
+              flat: true
+            },
+            VOverlay: {
+              elevation: 0
+            }
+          },
+          locale: {
+            locale: 'en',
+            fallback: 'en',
+          },
+        }
+      }))
+
+      await installModule('vuetify-nuxt-module')
+
+    }
 
     {
 
@@ -16,58 +67,12 @@ export default defineNuxtModule({
     {
 
       await installModule('@nuxt/icon')
-      await installModule('vuetify-nuxt-module')
 
     }
 
     addComponentsDir({
       path: resolve('./runtime/components')
     });
-
-    nuxt.hook('vuetify:registerModule', register => register({
-      moduleOptions: {
-        styles: 'sass',
-      },
-      vuetifyOptions: {
-        labComponents: ['VPullToRefresh'],
-        theme: {
-          defaultTheme: 'dark',
-        },
-        defaults: {
-          VBtn: {
-            flat: true
-          },
-          VCard: {
-            flat: true
-          },
-          VList: {
-            flat: true
-          },
-          VListItem: {
-            flat: true
-          },
-          VField: {
-            flat: true
-          },
-          VTextField: {
-            flat: true
-          },
-          VTextarea: {
-            flat: true
-          },
-          VMenu: {
-            flat: true
-          },
-          VOverlay: {
-            elevation: 0
-          }
-        },
-        locale: {
-          locale: 'en',
-          fallback: 'en',
-        },
-      }
-    }))
 
   }
 });
