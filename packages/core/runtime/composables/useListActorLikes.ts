7ad6168cbaa2e36f7d655c0ff28e5df2e56729b4
dxlliv <hello@dxlliv.dev>
2025-06-11 23:41:57 +0200

    fix: Fix list actor and bookmarks

--- packages/core/runtime/composables/useListActorLikes.ts
+++ packages/core/runtime/composables/useListActorLikes.ts
@@ -4,44 +4,52 @@ import { useListFeedWorker } from '@gridsky/core/runtime/composables/useListFeed
 import { useAgent } from '@gridsky/core/runtime/composables/useAtproto';
 import { ThreadModel } from '@gridsky/core/runtime/models/ThreadModel';
 import { useAccountStore } from '@gridsky/core/runtime/stores/storeAccount';
+import {useThreadModel} from "@gridsky/core/runtime/composables/useThreadModel";
+import {useListFeedChunkLoader} from "@gridsky/core/runtime/composables/useListFeedChunkLoader";
+import {useListNavigation} from "@gridsky/core/runtime/composables/useListNavigation";
 
 export function useListActorLikes() {
-    const baseList = useListBase();
-    const { cursor, resetCursor, updateCursor } = useListCursor();
     const accountStore = useAccountStore();
 
     const workerConfig = {
-        fetcher: {
-            method: 'app.bsky.feed.getActorLikes',
-            options: {
-                actor: accountStore.account.did,
-                limit: 100,
-            },
+      parser: {
+        listKey: 'feed',
+        actions: {
+          resetCursor: false
         },
-        processor: {
-            listKey: 'feed',
-        },
-        storage: {
-            context: 'gridsky:common',
-            name: 'ProfileLikes',
-            key: accountStore.account.did,
-        },
+      },
+      storage: {
+        context: 'gridsky:common',
+        name: 'ProfileLikes',
+        key: accountStore.account.did,
+      },
     };
 
+    const baseList = useListBase();
+    const { cursor, resetCursor, updateCursor } = useListCursor();
     const { worker, postMessage, listenOnce } = useListFeedWorker(workerConfig);
 
     const _this: any = {
         ...baseList,
         fetchList,
+        refetchList,
         requestItems,
         cursor,
         worker,
     };
 
-    async function requestItems(resetCursorFlag?: boolean) {
-        if (resetCursorFlag) {
+    const chunkLoader = useListFeedChunkLoader(_this);
+    _this.chunkLoader = chunkLoader
+
+    const navigation = useListNavigation(_this)
+    _this.navigation = navigation
+
+    async function requestItems(reset?: boolean) {
+        if (reset) {
             resetCursor();
         }
+
+        workerConfig.parser.actions.resetCursor = !!reset
 
         const response = await useAgent('auto').app.bsky.feed.getActorLikes({
             actor: accountStore.account.did,
@@ -49,45 +57,60 @@ export function useListActorLikes() {
             cursor: cursor.value,
         });
 
-        if (response.data?.cursor) {
+        // why does it return a cursor if it has reached end?
+        // let me handle that
+        if (response.data.feed.length < 100) {
+          delete response.data.cursor
+        }
+
+        if (response.data && response.data.cursor) {
             updateCursor(response.data.cursor);
         }
 
-        postMessage('fetch', {
-            meta: {
-                overrideConfig: {
-                    fetcher: {
-                        response,
-                    },
-                },
-            },
+        postMessage({
+          type: 'process',
+          config: workerConfig,
+          response
         });
 
         return new Promise((resolve) => {
-            listenOnce('fetch', async (e) => {
+            listenOnce('processed', async (e) => {
                 await appendItems(e.data.result);
+                chunkLoader.preloadNextChunk('FeedModel');
                 resolve(true);
             });
         });
     }
 
     async function appendItems(data: { hasReachedEnd: boolean; items: any[] }) {
-        baseList.prepareToAppendItems();
+        baseList.prepareToAppendItems()
 
-        let index = baseList.list.value.length;
+        // convert each item in ThreadModel and populate list
+        const threadsInstanced: ThreadModel[] = []
 
-        const threadsInstanced: ThreadModel[] = data.items.map((item: any) => {
-            return new ThreadModel(item, index++);
-        });
+        let index = baseList.list.value.length
 
-        data.items = threadsInstanced;
+        data.items.forEach((thread: any) => {
+          const threadInstance = useThreadModel(thread, index);
+          threadsInstanced.push(threadInstance);
+          index++
+        })
 
-        await baseList.appendItems(data);
+        // overwrite items with instanced items
+        data.items = threadsInstanced
+
+        // call parent method to append lists
+        await baseList.appendItems(data)
     }
 
     async function fetchList(restart?: boolean) {
-        baseList.isLoading.value = true;
-        await requestItems(restart);
+      baseList.isLoading.value = true;
+      await requestItems(restart);
+      baseList.isLoading.value = false;
+    }
+
+    async function refetchList() {
+      await fetchList(true);
     }
 
     return _this;
---
+++ packages/core/runtime/composables/useListProfileBookmark.ts
@@ -0,0 +1,128 @@
+import {useListBase} from './useListBase';
+import {useListCursor} from './useListCursor';
+import {useListNavigation} from './useListNavigation';
+import {useListFeedWorker} from './useListFeedWorker';
+import {useListFeedChunkLoader} from './useListFeedChunkLoader';
+import {useAccountSessionStore} from '@gridsky/core/runtime/stores/storeAccountSession'
+import {useThreadModel} from "@gridsky/core/runtime/composables/useThreadModel";
+import {useAgent} from './useAtproto';
+import {BookmarkRecord} from "@/lex";
+
+export function useListProfileBookmark(source) {
+  const accountSessionStore = useAccountSessionStore()
+
+  const workerConfig = {
+    parser: {
+      listKey: 'posts',
+      actions: {
+        resetCursor: false
+      },
+      normalizeItems: {
+        putInObjectProperty: 'post',
+      },
+    },
+    storage: {
+      context: 'gridsky:common',
+      name: 'ProfileBookmarks',
+      key: accountSessionStore.activeDid,
+    },
+  }
+
+  const baseList = useListBase(source);
+  const {cursor, resetCursor, updateCursor} = useListCursor();
+  const {worker, postMessage, listenOnce} = useListFeedWorker(workerConfig);
+
+  const _this: any = {
+    ...baseList,
+    fetchList,
+    refetchList,
+    requestItems,
+    cursor,
+    worker,
+  }
+
+  const chunkLoader = useListFeedChunkLoader(_this);
+  _this.chunkLoader = chunkLoader
+
+  const navigation = useListNavigation(_this)
+  _this.navigation = navigation
+
+  async function requestItems(reset?: boolean) {
+    if (reset) {
+      resetCursor();
+    }
+
+    workerConfig.parser.actions.resetCursor = !!reset
+
+    const agent = new BookmarkRecord(useAgent("private"));
+
+    const response = await agent.list({
+      repo: accountSessionStore.activeDid,
+      cursor: cursor.value,
+      limit: 50,
+    });
+
+    const bookmarkRecords = response.records.filter(record => !!record.value?.subject);
+    const postUris = bookmarkRecords.map(record => record.value.subject);
+
+    if (response.cursor) {
+      updateCursor(response.cursor);
+    }
+
+    if (postUris.length === 0) {
+      return;
+    }
+
+    const postsResponse = await useAgent('auto').app.bsky.feed.getPosts({
+      uris: postUris,
+    });
+
+    postMessage({
+      type: 'process',
+      config: workerConfig,
+      response: postsResponse,
+    });
+
+    return new Promise((resolve) => {
+      listenOnce('processed', async (e) => {
+        await appendItems(e.data.result);
+        chunkLoader.preloadNextChunk('FeedModel');
+        resolve(true);
+      });
+    });
+  }
+
+  async function appendItems(data: { hasReachedEnd: boolean, items: any[] }) {
+    baseList.prepareToAppendItems()
+
+    // convert each item in ThreadModel and populate list
+    const threadsInstanced: ThreadModel[] = []
+
+    let index = baseList.list.value.length
+
+    data.items.forEach((thread: any) => {
+      const threadInstance = useThreadModel(thread, index);
+      threadsInstanced.push(threadInstance);
+      index++
+    })
+
+    // overwrite items with instanced items
+    data.items = threadsInstanced
+
+    // call parent method to append lists
+    await baseList.appendItems(data)
+  }
+
+  async function fetchList(restart?: boolean) {
+    baseList.isLoading.value = true;
+    await requestItems(restart);
+    baseList.isLoading.value = false;
+  }
+
+  async function refetchList() {
+    await fetchList(true);
+  }
+
+
+  return _this
+}
