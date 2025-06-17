94570ac0ac8a9e4d57fb06f20950ec7d5b6aef84
dxlliv <hello@dxlliv.dev>
2025-06-17 07:47:22 +0200

    chore: Add appearance.ts config and state.t consts

---
+++ packages/core/runtime/config/appearance.ts
@@ -0,0 +1,20 @@
+export const appAppearanceConfig = {
+    theme: {
+        name: 'gridgram',
+        variant: '',
+        dark: true,
+        colorPrimary: '#0095f6',
+        backgroundTone: ''
+    },
+    banner: {
+        enabled: true,
+        gridskyColors: [
+            '#0069ff',
+            '#333333'
+        ],
+        gridskyEnabled: true,
+        blueskyEnabled: false,
+        shadertoyEnabled: false,
+        shadertoyId: '',
+    }
+}
---
+++ packages/core/runtime/consts/state.ts
@@ -0,0 +1,3 @@
+export const STATE_PROFILE_GLOBAL = 'profileGlobal'
+export const STATE_THREAD_REPLY_PARENT = 'threadReplyParentGlobalState'
+export const STATE_BOTTOM_SHEET_GLOBAL = 'bottomSheetGlobal'
